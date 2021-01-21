1edee34e9f57d507cd0e291a17af37320ce02d22
ashikmahmud <ashikmamud187@gmail.com>
2021-01-22 01:00:48 +0600

    You have adding message

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    // Typed.js initialize 
+    var typed = new Typed('#type', {
+        strings: [
+        'Ashik Mahmud',
+        'Freelancer', 
+        'Web Designer',
+        'Web Developer',
+        'Marketing Host',
+        'Entrepreneur '
+        ],
+        typeSpeed:40,
+        backDelay:1000,
+        backSpeed:40,
+        loop:true
+      });
+      var typed2 = new Typed('.typed', {
+        strings: [
+        'have always been ignore joy in life.',
+        'am always try to do not go people convention', 
+        'was always be secure of Environment things Like Love,Everthing'
+        
+        ],
+        typeSpeed:40,
+        backDelay:1000,
+        backSpeed:40,
+        loop:true
+      });
+})
